aa2a7cdc9694aa7f4e59e57c8c53d0242b9a50d1
Ivan Kondzerau <konderev.ivan@gmail.com>
2020-02-21 11:58:51 +0100

    feat: towel-sort

--- src/index.js
+++ src/index.js
@@ -2,5 +2,16 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if (matrix === undefined || matrix.length === 0) {
+      return [];
+  }
+  var ARR = [];
+  
+  for (var i = 0; i < matrix.length; i += 2) {
+    ARR = ARR.concat(matrix[i]);
+    if (i + 1 < matrix.length) {
+        ARR = ARR.concat(matrix[i + 1].reverse());
+    }
+  }
+  return ARR;
 }
